02601e6d8eb81c6b0e47ed7e33697a8243705bd6
crotman <crotman@gmail.com>
2019-10-19 17:51:44 -0300

    commit alert reader

--- analyze.R
+++ analyze.R
@@ -1,16 +1,47 @@
 library(git2r)
 library(tidyverse)
+library(magrittr)
+library(furrr)
+library(uuid)
 
 repository <- repository("repository/Twitter4j")
 
+iteracao <- 0
 
+check_out_and_pmd <- function(commit, id){
 
-#check_out_and_pmd <- function()
+    iteracao <<- iteracao + 1
+    print(iteracao)
+    temp <- (paste0(UUIDgenerate(),".csv"))
+    print(commit$sha)    
+    checkout(commit, force = TRUE)    
+    shell(paste0("pmd/bin/pmd.bat -d repository/Twitter4J -f csv -R rulesets/java/quickstart.xml -cache cache -reportfile ", temp), shell = "PowerShell")
+    conteudo <- read_csv(temp) %>% 
+        mutate(sha = commit$sha)
+    file.remove(temp)
+    conteudo
+
+}
 
 
 commits <-  commits(repository) %>% 
     map_df(as_tibble) %>% 
-    head()
+    head(100) 
+
+saveRDS(commits, "commits.rds")
+
+commits_alerts <-  commits(repository) %>% 
+    head(100) %>% 
+    map_dfr(check_out_and_pmd)
+    
+saveRDS(commits_alerts, "commits_alerts.rds")
+
+
+
+
+
+
+
 
 
 
